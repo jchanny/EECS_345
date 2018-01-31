b1c9f69322e9566ec04dccec540a417b319c6304
Jeremy <jsc126@case.edu>
2018-01-31 08:48:40 -0500

    sync changes

--- hw1.scm
+++ hw1.scm
@@ -4,10 +4,10 @@
 ;; params: number n, sorted list lis
 (define insert
   (lambda (n lis)
-	(cond
-	 ((null? lis) '())
-	 ((< (car lis) n) (cons (car lis) (insert n (cdr lis))))
-	 ((>= (car lis) n) (cons n (cons (car lis) (cdr lis)))))))
+    (cond
+     ((null? lis) '())
+     ((< (car lis) n) (cons (car lis) (insert n (cdr lis))))
+     ((>= (car lis) n) (cons n (cons (car lis) (cdr lis)))))))
 
 
 ;;#2: Merge: returns a sorted combination of two lists
@@ -36,13 +36,51 @@
      (else (cons (car lis) (removedups (cdr lis))))
      )))
 
+;;#5: deepcons: places letter in front of leftmost non-list atom
+;; params: x: letter to be placed; lis: list containing sublists and atoms
+(define deepcons
+  (lambda (x lis)
+    (cond
+     ((null? lis) '())
+     ((list? (car lis))
+      ( cons (deepcons x (car lis)) (cdr lis)))
+     (else (cons x lis))
+     )))
+
+;;#6: numparens: counts number of balanced parantheses in list
+;;params: lis: list containing atoms
+(define numparens
+  (lambda (lis)
+    (cond
+     ((null? lis) 1)
+     ((list? (car lis))
+      (+ (+ 0 (numparens(car lis))) (+ 0 (numparens(cdr lis)))))
+     (else (numparens (cdr lis)))
+     )))
 
 ;;#7: dup*: returns a list with all atoms in the list duplicated
 ;; params: lis: list of atoms, may include sublists
 (define dup*
   (lambda (lis)
     (cond
-      ((null? lis) '())
-      ((list? (car lis)) (cons (cons(dup*(car lis)) (dup*(car lis))) (dup*(cdr lis))))
-      (else (cons (car lis) (cons(car lis) (dup*(cdr lis)))))
-      )))
+     ((null? lis) '())
+     ((list? (car lis))
+      (cons (cons (dup* (car lis)) (dup* (car lis))) (dup* (cdr lis))))
+     (else (cons (car lis) (cons (car lis) (dup* (cdr lis)))))
+     )))
+
+;;#8: removedups* INCOMPLETE: removes repeating atoms from sublists
+;;params: lis: list of atoms, may include sublists
+(define removedups*
+  (lambda (lis)
+    (cond
+     ((null? lis)'())
+     ((null? (cdr lis)) lis)
+     ((list?(car lis))
+      (cons(removedups*(car lis))(removedups*(cdr lis))))
+     ((eq? (car lis)(car (cdr lis)))
+      (removedups*(cdr lis)))
+     (else (cons (car lis)(removedups*(cdr lis))))
+     )))
+
+;;#10: removedups**
