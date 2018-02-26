7e19aafc21c4e96f0d0c724c42db3927b48274e4
Jeremy <jsc126@case.edu>
2018-02-26 01:39:00 -0500

    halfway done

--- prog2.scm
+++ prog2.scm
@@ -13,3 +13,54 @@
 
 ;;#2: merge: combines 2 lists together, sorted in correct oder
 ;;params: l-list 1; r-list 2
+(define merge-cps
+  (lambda (l r return)
+    (cond
+     ((null? l) (return r))
+     ((null? r) (return l))
+     ((< (car l) (car r)) (merge-cps (cdr l) r (lambda (v) (return (cons (car l) v)))))
+     (else (merge-cps l (cdr r) (lambda (v) (return (cons (car r) v))))))))
+
+(define merge
+  (lambda (l r)
+    (merge-cps l r (lambda (v) v))))
+
+;;#3: removedups: removes successive repeating elements from a list
+;;params: lis: list of atoms
+(define removedups-cps
+  (lambda (lis return)
+    (cond
+     ((null? lis) (return lis))
+     ((null? (cdr lis)) (return lis))
+     ((eq? (car lis) (cadr lis)) (removedups-cps (cdr lis) (lambda (v) (return v))))
+     (else (removedups-cps (cdr lis) (lambda (v) (return (cons (car lis) v))))))))
+
+(define removedups
+  (lambda (lis)
+    (removedups-cps lis (lambda (v) v))))
+
+;;#4: numparens: returns number of parentheses pairs in a list
+;;params: lis: list containing atoms and parentheses
+(define numparens-cps
+  (lambda (lis return)
+    (cond
+     ((null? lis) (return 1))
+     ((list? (car lis)) (numparens-cps (car lis) (lambda (v1) (numparens-cps (cdr lis) (lambda (v2) (return (+ v1 v2)))))))
+     (else (numparens-cps (cdr lis) return)))))
+
+(define numparens
+  (lambda (lis)
+    (numparens-cps lis (lambda (v) v))))
+
+;;#5: dup*: duplicates contents and sublists
+;;params: lis: list of atoms, including sublists
+(define dup*-cps
+  (lambda (lis return)
+    (cond
+     ((null? lis) (return lis))
+     ((list? (car lis)) (dup*-cps (car lis) (lambda (v1) (dup*-cps (car lis) (lambda (v2) (dup*-cps (cdr lis) (lambda (v3) (return (cons v1 (cons v2 v3))))))))))
+     (else (dup*-cps (cdr lis) (lambda (v) (return (cons (car lis) (cons (car lis) v)))))))))
+
+(define dup*
+  (lambda (lis)
+    (dup*-cps lis (lambda (v) v))))
