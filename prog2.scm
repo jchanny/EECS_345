40d2f9a08206566c0d5eba5d151015e5577d32a1
Jeremy Chan <jsc126@case.edu>
2018-02-28 22:50:47 -0500

    all but 2 work perfect

--- prog2.scm
+++ prog2.scm
@@ -85,9 +85,9 @@
 (define split-cps
   (lambda (lis return)
     (cond
-      ((null? lis) (return '() '()))
-      ((null? (cdr lis)) (return lis '()))
-      (else (split-cps (cddr lis) (lambda (v1 v2) (return (cons (car lis) v1) (cons (cadr lis) v2))))))))
+	 ((null? lis) (return '() '()))
+	 ((null? (cdr lis)) (return lis '()))
+	 (else (split-cps (cddr lis) (lambda (v1 v2) (return (cons (car lis) v1) (cons (cadr lis) v2))))))))
 
 (define merge-cps
   (lambda (l r return)
@@ -97,11 +97,60 @@
      ((< (car l) (car r)) (merge-cps (cdr l) r (lambda (v) (return (cons (car l) v)))))
      (else (merge-cps l (cdr r) (lambda (v) (return (cons (car r) v))))))))
 
-split continuation: (lambda (v1 v2) (list v1 v2))
+(define mergesort-cps
+  (lambda (lis return)
+    (cond
+      ((null? lis) lis)
+      ((null? (cdr lis)) lis)
+      (else (merge-cps (mergesort-cps (car (split-cps lis (lambda (v1 v2) (list v1 v2)))) (lambda (v) v))
+                       (mergesort-cps (cadr (split-cps lis (lambda (v1 v2) (list v1 v2)))) (lambda (v) v))
+                       (lambda (v) v))))))
+
+(define mergesort
+  (lambda (lis)
+    (mergesort-cps lis (lambda (v) v))))
+
+;;8: replaceatoms: replaces list2.length # of atoms in list1 with those of list 2
+;;params: l: list 1; r: list 2
+(define replaceatoms-cps
+  (lambda (l r return)
+    (cond
+      ((null? l) (return '()))
+      ((null? r) (return l))
+      ((list? (car l)) (replaceatoms-cps (car l) r (lambda (v) (replaceatoms-cps (cdr l) (cdr r) (lambda (v2) (return (cons v v2)))))))
+      (else (replaceatoms-cps (cdr l) (cdr r) (lambda (v) (return (cons (car r) v))))))))
+
+(define replaceatoms
+  (lambda (l r)
+    (replaceatoms-cps l r (lambda (v) v))))
 
 ;;9: suffix: returns list containing elements after last occurence of a specified atom
 ;;params: lis: list of atoms, x: specified atom
 (define suffix-helper
-  (lambda (lis x)
+  (lambda (x lis break)
     (cond
-     ((null? lis)
+	 ((null? lis) lis)
+	 ((eq? x (car lis)) (break (suffix x (cdr lis))))
+	 (else (cons (car lis) (suffix-helper x (cdr lis) break))))))
+
+(define suffix
+  (lambda (x lis)
+    (call/cc
+     (lambda (break)
+       (suffix-helper x lis break)))))
+
+;;10: removes all elements from lists and sublists if they contain a certain atom
+;;params: lis: list of atoms, including sublists; x: specified atom
+(define emptysublists-helper
+  (lambda (x lis break)
+    (cond
+	 ((null? lis) lis)
+	 ((list? (car lis)) (cons (emptysublists x (car lis)) (emptysublists x (cdr lis))))
+	 ((eq? (car lis) x) (break '()))
+	 (else (cons (car lis) (emptysublists-helper x (cdr lis) break))))))
+
+(define emptysublists
+  (lambda (x lis)
+    (call/cc
+     (lambda (break)
+       (emptysublists-helper x lis break)))))
